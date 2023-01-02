e0a73e1138652f27ecd59f0a27ba58e881a988b4
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-01-02 23:28:05 +0500

    home page

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,17 +2,19 @@
 import '@/@fake-db/db'
 import '@/@iconify/icons-bundle'
 import App from '@/App.vue'
+import ability from '@/plugins/casl/ability'
 import i18n from '@/plugins/i18n'
 import layoutsPlugin from '@/plugins/layouts'
 import vuetify from '@/plugins/vuetify'
 import { loadFonts } from '@/plugins/webfontloader'
 import router from '@/router'
+import { abilitiesPlugin } from '@casl/vue'
 import '@core-scss/template/index.scss'
 import '@styles/styles.scss'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import ability from '@/plugins/casl/ability'
-import { abilitiesPlugin } from '@casl/vue'
+import './assets/scss/style.scss'
+
 loadFonts()
 
 
